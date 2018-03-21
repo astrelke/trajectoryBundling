47c492323c9988d6d4bf9c4d8b77f6b2af4f21f1
astrelke <Strelke.Andrew@gmail.com>
2018-03-21 12:14:15 -0700

    Update script.js

--- example/script.js
+++ example/script.js
@@ -1,23 +1,24 @@
 var fs = require('fs');
 var geojson2mvt = require('../src');
+var json;
 
-var json = JSON.parse(fs.readFileSync('simpleLines0.geojson', "utf8"))
-//var simplify = require('simplify-geojson')
-//var simplified = simplify(json, 0.1)
-
-var options = {
-  layers: {
-    layer0: json,
-  },
-  rootDir: 'simpleLines',
-  bbox : [-15.515873,-71.999992,85.80096,-66.0], //[south,west,north,east]
-  zoom : {
-    min : 0,
-    max : 0
-  },
-  tolerance : 6
-};
-console.time('geojson2mvt');
-// build the static tile pyramid
-geojson2mvt(options);
-console.timeEnd('geojson2mvt');
+for(var i=0; i<=10; i++){
+	console.log('simpleLines'+i.toString()+'.geojson');
+	json = JSON.parse(fs.readFileSync('simpleLines'+i.toString()+'.geojson', "utf8"))
+	var options = {
+	  layers: {
+		layer0: json,
+	  },
+	  rootDir: 'simpleLines',
+	  bbox : [-15.515873,-75.999992,85.80096,0.0], //[south,west,north,east]
+	  zoom : {
+		min : i,
+		max : i
+	  },
+	  tolerance : 6
+	};
+	console.time('geojson2mvt'+i.toString());
+	// build the static tile pyramid
+	geojson2mvt(options);
+	console.timeEnd('geojson2mvt'+i.toString());
+}
