f27ba033aa71fc450464e6ad69879b1920eff289
astrelke <Strelke.Andrew@gmail.com>
2018-04-17 09:53:04 -0700

    Full update of Traclus

--- example/traclus.js
+++ example/traclus.js
@@ -1,4 +1,12 @@
-/*TRACLUS ALGORITHM*/
+/* TRACULUS Algorithm: 
+	First introduced by Jae-Gil Lee, Jiawei Han, and Kyu-Young Whang
+	http://hanj.cs.illinois.edu/pdf/sigmod07_jglee.pdf
+*/
+
+//Global Variables
+var coefficient;
+var projPoint;
+//Setup Variables
 var fs = require('fs');
 var T = [], features=[];
 var feature;
@@ -9,18 +17,16 @@ json.features.forEach(function(feat){
 });
 var result = TRACULUS(T);
 for(var i=0;i<result.length;i++){
-	console.log("Result : " + result[i]);
-	feature = {"type":"Feature", "geometry":{"type":"LineString", "coordinates":result[i]}, "properties":{"name":0}};
-	features.push(feature);
+		feature = {"type":"Feature", "geometry":{"type":"LineString", "coordinates":result[i]}, "properties":{"name":0}};
+		features.push(feature);
 }
-var newJson = {"type":"FeatureCollection","features":features};4
+console.log(result.length);
 //OUTPUT GEOJSON
+var newJson = {"type":"FeatureCollection","features":features};
 var fileName = 'BestTrack_2.geojson';
 fs.writeFileSync(fileName, JSON.stringify(newJson));
 
-
-/* TRACULUS Algorithm: http://hanj.cs.illinois.edu/pdf/sigmod07_jglee.pdf
-	First introduced by Jae-Gil Lee, Jiawei Han, and Kyu-Young Whang
+/* TRACULUS
 	Inputs (T: Trajectories)
 	Output (R: Representative Trajectory) 
 */
@@ -40,37 +46,37 @@ function TRACULUS(T){
 	console.log("Paritioning Phase Started");
 	T.forEach(function(tr) {
 		//02 & 03: Get a set L of line segments and Accumulate L into a set D
-		ApproximateTrajectoryParitioning(tr,i,D); 
+		ApproximateTrajectoryParitioning(tr,i,D,25,50); 
 		console.log("Segment Group: " + i.toString());
 		i++;
 	});	
+	console.log(D.length);
 	console.log("Paritioning Phase Complete");
-	
 	/* Grouping Phase */
 	console.log("Grouping Phase Started");
 	//04: Get a set O of clusters
-	O = LineSegmentClustering(D,10,5,5,T);
+	O = LineSegmentClustering(D,30,6,6,T);
 	console.log("Clusters Generated");
 	//05: Loop through each cluster c
 	i=1;
 	O.forEach(function(C) {
 		//06: Get a represenative trajectory
-		var RTR = RepresentativeTrajectoryGeneration(C,5,1);
-		//console.log(RTR);
-		R.push(RTR);
-		console.log("Representative Trajectory: " + i.toString());
-		i++;
-	}); 
-	//R = RepresentativeTrajectoryGeneration(O[0],6,0.3, R);
+		var RTR = RepresentativeTrajectoryGeneration(C,6,4);
+		if(RTR.length>0){
+			R.push(RTR);
+			console.log("Representative Trajectory: " + i.toString());
+			i++;
+		}
+	}); 	
 	console.log("Grouping Phase Complete");
 	return R;
 }
 
 /* Approximate Trajectory Paritioning
-	Inputs (tr: Trajectory, i: Trajectory Index, D: Set of line segments)
+	Inputs (tr: Trajectory, i: Trajectory Index, D: Set of line segments,costAdv: Used to prevent oversimplification of line segment partitioning, MinLns: Minimum length of paritiooned line segment)
 	Output (D: Set of line segments) 
 */
-function ApproximateTrajectoryParitioning(tr,i,D){
+function ApproximateTrajectoryParitioning(tr,i,D,costAdv,MinLns){
 	/*Variables
 		Arrays (
 			line: Partitioned line segment
@@ -85,26 +91,32 @@ function ApproximateTrajectoryParitioning(tr,i,D){
 			costNopar: MDL cost of original line 
 		)
 	*/
-	var line=[]; var startIndex=0,len=1,currIndex; var costPar,costNopar;
+	var line=[]; var startIndex=0,len=1,currIndex; var costPar=0,costNopar=0;
 	line.push(tr[0]); //The starting point
 	while(startIndex+len < tr.length){
 		currIndex = startIndex+len;
+		if(getDistance(line[0],tr[currIndex-1]) < MinLns) {
+			len += 1;
+			continue;
+		}
 		costPar = MDLpar(tr, startIndex, currIndex);
-		costNopar = MDLnopar(tr, startIndex, currIndex);
+		costNopar += MDLnopar(tr, currIndex-1, currIndex);
 		/* check if partitioning at the current point makes
 		the MDL cost larger than not partitioning*/
-		if(costPar > costNopar){
+		if(costPar > costNopar+costAdv){
 			/*partition at the previous point*/
 			line.push(tr[currIndex-1]);
-			D.push({"L":line,"trajectory":i,"clusterId":0,"classified":false,"noise":false});
+			D.push({"L":line,"index": D.length,"trajectory":i,"clusterId":0,"classified":false,"noise":false});
 			line = [];
 			line.push(tr[currIndex-1]);
 			startIndex = currIndex - 1;
 			len = 1;
+			costPar=0;
+			costNopar=0;
 		} else len += 1;
 	}
 	line.push(tr[tr.length-1]);
-	D.push({"L":line,"trajectory":i,"clusterId":0,"classified":false,"noise":false});
+	D.push({"L":line,"index": D.length, "trajectory":i,"clusterId":0,"classified":false,"noise":false});
 }
 
 /* Line Segment Clustering
@@ -127,7 +139,7 @@ function LineSegmentClustering(D,e,MinLns,MinClus,T){
 	*/
 	var tempD,tempC,C=[],N,Q=[],O=[]; var clusterId = 0; 
 	/*Step 1:*/
-	for(var i=0; i<D.length-1;i++){ 
+	for(var i=0; i<D.length;i++){ 
 		if(!D[i].classified){
 			//Compute N(L) and predict clusters, where L=D[i]
 			tempD = D; tempC = C; 
@@ -168,22 +180,13 @@ function ExpandCluster(Q,D,C,clusterId,e,MinLns){
 		//Compute N(L), where L=Q[0]
 		N = Ne2(Q,0,e);
 		if(N.length >= MinLns){
-			N.forEach(function(X){
+			N.forEach(function(X){		//For each neighboring line X
 				//Assign clusterId to X
 				if(!X.classified || X.noise){
+						if(!X.classified) Q.push(X);	//Insert X into queue Q
 					X.clusterId = clusterId;
-					//Add point to cluster
-					if(X.noise && X.classified){
-						X.noise = false;
-						C[clusterId].push(X);
-						D[X.index] = X;
-					}
-				}
-				//Insert X into queue Q
-				if(!X.classified) {
 					X.classified = true;
 					X.noise = false;
-					Q.push(X);
 					C[clusterId].push(X);
 					D[X.index] = X;
 				}
@@ -232,33 +235,21 @@ function RepresentativeTrajectoryGeneration(C,MinLns,Y){
 	*/
 	var V,v2,C2=[],P=[],sweepLine,intersects,pAvg,pAvg2,RTR=[];
 	var intersect;
-	var i=0, j=0,pNum=0;
-	var x1=0.0,x2=0.0,y1=0.0,y2=0.0,angle,diff,xPrev=64666266609.0,y=0.0;
+	var i=0, j=0,pNum=0,lineNum=0,clusterNum=0;
+	var x1=0.0,x2=0.0,y1=0.0,y2=0.0,angle,diff,xPrev=0.0,y=0.0;
 	var lineIntersection = require('line-intersection');
-	var v3;
 	
 	//Compute average direction vector V
 	C.forEach(function(v){
 		x1 += v.L[0][0]; x2 += v.L[1][0]; y1 += v.L[0][1]; y2 += v.L[1][1];
 	});
-	//var R=[];
 	V = [[x1/C.length,y1/C.length],[x2/C.length,y2/C.length]];
-	//R.push(V);
 	//Calculate angle in which to rotate X axis
 	angle = angleBetween2Lines(V[0],[V[1][0],V[0][1]],V[0],V[1]);
-/* 	console.log(angle);
-	var axis = [[-200,0],[200,0]];
-	R.push(axis);
-	var newaxis = rotateMatrix(axis,angle);
-	R.push(newaxis); */
-	/*X'-value denotes the coordinate of the X' axis */
+	//X'-value denotes the coordinate of the X' axis
 	C.forEach(function(v){
 		//Rotate vector and add to rotated cluster C2
-		//R.push(v.L);
 		v2 = rotateVector(v,angle);
-		//R.push(v2.L)
-		//v3 = rotateVector(v,angle);
-		//R.push(v3.L);
 		C2.push(v2);
 		//If end point is before start point on X'-axis, swap points
 		if(v2.L[1][0] < v2.L[0][0]) { v2.L = [v2.L[1],v2.L[0]];}
@@ -276,32 +267,34 @@ function RepresentativeTrajectoryGeneration(C,MinLns,Y){
 		}
 		//Reset indicies
 		i=0; j=0;
+		//Increment lineNum
+		lineNum++;
 	});
 	i=0;
+	
 	P.forEach(function(p){
-		/*Count pNum using a sweep line (or plane) */
-		sweepLine = [[p[0],p[1]+10],[p[0],p[1]-10]];
-		//R.push(sweepLine);
+		//Count pNum using a sweep line (or plane) 
+		sweepLine = [[p[0],p[1]+20],[p[0],p[1]-20]];
 		intersects=[];
 		C2.forEach(function(v){
 			if((v.L[0][0] <= sweepLine[0][0] && sweepLine[0][0] <= v.L[1][0]) || (v.L[0][0] >= sweepLine[0][0] && sweepLine[0][0] >= v.L[1][0])){
 				intersect = lineIntersection.findIntersection([{x: v.L[0][0], y: v.L[0][1]}, {x: v.L[1][0], y: v.L[1][1]}, 
 					{x: sweepLine[0][0], y: sweepLine[0][1]}, {x: sweepLine[1][0], y: sweepLine[1][1]}]);
 				pNum++;
-				intersects.push([intersect.x,intersect.y]);
+				if(!(isNaN(intersect.x) || isNaN(intersect.y)))		//Check if intersection exisits 
+					intersects.push([intersect.x,intersect.y]);
 			}
 		});
 		if(pNum >= MinLns){
 			//Compute diff
-			if(xPrev == 64666266609.0) diff = Y+1;	//Hack conditional statement below
-			else diff = p[0] - xPrev;
+			diff = Math.abs(p[0] - xPrev);
 			if(diff >= Y){
 				xPrev = p[0];
 				//Compute the average coordinate pAvg2
 				intersects.forEach(function(intxn){ y += intxn[1]; });
 				pAvg2 = [p[0], y/intersects.length];
 				//Undo the rotation and get the point pAvg2
-				pAvg = rotatePoint(pAvg2,angle)
+				pAvg = rotatePoint(pAvg2,angle);
 				//Append Pavg to the end of RTRi
 				RTR.push(pAvg);
 			}
@@ -310,6 +303,7 @@ function RepresentativeTrajectoryGeneration(C,MinLns,Y){
 		i++;
 		y=0.0; pNum=0;
 	});	
+	console.log();
 	return RTR;
 }
 
@@ -318,25 +312,31 @@ function RepresentativeTrajectoryGeneration(C,MinLns,Y){
 */
 /*Get MDL cost with partition*/
 function MDLpar(tr, startIndex, currIndex){
-	var H, D_H;
-	//Calc L(H)
-	H = Math.log(getDistance(tr[startIndex],tr[currIndex]));
-	//Calc L(D|H)
+	var H=0, D_H=0;
 	var angularDistance=0.0, perpDistance=0.0;
+	//Calc L(H)
+	H = MDLnopar(tr, startIndex, currIndex);
+	//Calc L(D|H)
 	for(var i=startIndex; i<currIndex; i++){
 		//Calc perpendicular distance
-		perpDistance += calcPerpDistance([tr[startIndex],tr[currIndex]],[tr[i],tr[i+1]]);
+		perpDistance = calcPerpDistance([tr[startIndex],tr[currIndex]],[tr[i],tr[i+1]]);
 		//Calc angular distance
-		angularDistance += calcAngularDistance([tr[startIndex],tr[currIndex]],[tr[i],tr[i+1]]);
+		angularDistance = calcAngularDistance([tr[startIndex],tr[currIndex]],[tr[i],tr[i+1]]);
+		if(perpDistance < 1.0) perpDistance = 1.0;
+		if(angularDistance < 1.0) angularDistance = 1.0;
+		D_H += Math.log2(perpDistance) + Math.log2(angularDistance);
 	}
-	D_H = Math.log(perpDistance) + Math.log(angularDistance);
-	return H + D_H;
+	return D_H;
 }
 /*Get MDL cost with no partition*/
 function MDLnopar(tr, startIndex, currIndex){
-	var len=0.0;
-	for(var i=startIndex; i<currIndex; i++) len += getDistance(tr[i],tr[i+1]);
-	return Math.log(len);
+	var H=0;
+	var d=0.0;
+	//Calc L(H)
+	d = getDistance(tr[startIndex],tr[currIndex]);
+	if(d < 1.0)
+		d = 1.0;	//for logarithmic function
+	return Math.log2(d);
 }
 /*Participating Trajectories Function*/
 function PTR(C,T){
@@ -362,76 +362,92 @@ function getDistance(p1,p2){
 }
 /*Calculate Perpendicular Distance*/
 function calcPerpDistance(Li,Lj){
-	var distance = require('euclidean-distance');
-	var projPoints;
-	//Calculate Projection Points
-	projPoints = calcProjPoint(Li, Lj);
 	//Calculate euclidean Distance
-	var l1 = distance(Lj[0],projPoints[0]);
-	var l2 = distance(Lj[1],projPoints[1]);
+	var l1 = getDistanceFromPointToLineSegment(Li,Lj[0]);
+	var l2 = getDistanceFromPointToLineSegment(Li,Lj[1]);
+	if(l1 == 0.0 && l2 == 0.0) return 0.0;
 	//Return perpendicular distance
-	return ((l1*l1 + l2*l2)/(l1 + l2));
+	return ((Math.pow(l1,2) + Math.pow(l2,2)) / (l1+l2));
 }
 /*Calculate Angular Distance*/
 function calcAngularDistance(Li,Lj){
-	var len,angle;
-	len = getDistance(Lj[0],Lj[1]);
-	angle = angleBetween2Lines(Lj[0],Lj[1], Li[0], Li[1]);
-	if(angle >= 0 && angle < 90) 
-		return len * Math.sin(angle);
-	else
-		return len;
+	var len1,len2,angle;
+	var p1 = [Li[1][0]-Li[0][0], Li[1][1]-Li[0][1]];
+	var p2 = [Lj[1][0]-Lj[0][0], Lj[1][1]-Lj[0][1]];
+	//get vector length
+	len1 = computeLength(p1);
+	len2 = computeLength(p2);
+	if(len1 == 0.0 || len2 == 0.0) return 0.0;
+	var innerProduct = computeInnerProduct(p1,p2);
+	var cosTheta = innerProduct / (len1 * len2);
+	if(cosTheta > 1.0) cosTheta = 1.0;
+	if (cosTheta < -1.0) cosTheta = -1.0;
+	var sinTheta = Math.sqrt(1-Math.pow(cosTheta,2));
+	return (len2 * sinTheta);
 }
-/*Calculate Parallel Distance*/
-function calcParallelDistance(Li,Lj){
-	var distance = require('euclidean-distance');
-	var projPoints;
-	var l1,l2,e1,e2;
-	//Calculate Projection Points
-	projPoints = calcProjPoint(Li, Lj);
-	/*Calculate minimum Euclidean distance between 
-	start vertex of Li and projected points*/
-	e1 = distance(Li[0],projPoints[0]);
-	e2 = distance(Li[0],projPoints[1]);
-	l1 = Math.min(e1,e2);
-	/*Calculate minimum Euclidean distance between 
-	end vertex of Li and projected points*/
-	e1 = distance(Li[1],projPoints[0]);
-	e2 = distance(Li[1],projPoints[1]);
-	l2 = Math.min(e1,e2);
-	//Calculate parallel distance
-	return Math.min(l1,l2);
+//Compute length of vector
+function computeLength(p){
+	var sum=0.0;
+	sum += Math.pow(p[0],2);
+	sum += Math.pow(p[1],2);
+	return Math.sqrt(sum);
 }
-/*Calculate projection point of line 1 onto line 2*/
-function calcProjPoint(Li, Lj){
-	var v = [], d=[], projPoint=[];
-	var x, y;
-	//Check which direction the line is going
-	var dir=0;	//0 = horziontal, 1 = vertical;
-	x = Math.abs(Li[1][0] - Li[0][0]);
-	y = Math.abs(Li[1][1] - Li[0][1]);
-	if(x < y)
-		dir = 1;
-	d[0] = Lj[0][dir] - Li[0][dir];
-	d[1] = Lj[1][dir] - Li[0][dir];
-	//Calculate projection points ps and pe
-	for(var i=0; i<2;i++){
-		//Construct vector and calculate distance
-		v.push(Li[1][0]-Li[0][0]);
-		v.push(Li[1][1]-Li[0][1]);
-		var vLen = Math.sqrt(v[0]*v[0] + v[1]*v[1]);
-		//Calculate normalization factor
-		var u = [[v[0]/vLen],[v[1]/vLen]];
-		//Calculate projection point
-		x = Li[0][0] + (d[i]*u[0]);
-		y = Li[0][1] + (d[i]*u[1]);
-		projPoint.push([x,y]);
-	}
-	return projPoint;
+//Get distance between a line segment and a point
+function getDistanceFromPointToLineSegment(v, p){
+	var p1 = [p[0]-v[0][0], p[1]-v[0][1]];
+	var p2 = [v[1][0]-v[0][0], v[1][1]-v[0][1]];
+	//Calculate coefficient
+	coefficient = computeInnerProduct(p1,p2) / computeInnerProduct(p2,p2);
+	//Calculate projection point
+	var x = v[0][0] + (coefficient * p2[0]);
+	var y = v[0][1] + (coefficient * p2[1]);
+	projPoint = [x,y];
+	var d = getDistance(p,projPoint);
+	return d;
+}
+//Dot Product Calculation
+function computeInnerProduct(p1,p2){
+	var innerProduct = 0.0;
+	innerProduct += (p1[0] * p2[0]);
+	innerProduct += (p1[1] * p2[1]);
+	return innerProduct;
 }
 /*Distance function described in text*/
-function dist(Li,Lj,wPerp,wPara,wAngle){
-	return wPerp * calcPerpDistance(Li,Lj) + wPara * calcParallelDistance(Li,Lj) + wAngle * calcAngularDistance(Li,Lj);
+function dist(Li,Lj){
+	var perpDistance,paraDistance, angleDistance;
+	var perpDistance1,perpDistance2,paraDistance1,paraDistance2,len1,len2;
+	//Compute Perpendicular Distance
+	len1 = getDistance(Li[0],Li[1]);
+	len2 = getDistance(Lj[0],Lj[1]);
+	if(len1 > len2){
+		perpDistance1 = getDistanceFromPointToLineSegment(Li,Lj[0]);
+		if(Math.abs(coefficient) < 0.5) paraDistance1 = getDistance(Li[0], projPoint);
+		else paraDistance1 = getDistance(Li[1],projPoint);
+		perpDistance2 = getDistanceFromPointToLineSegment(Li,Lj[1]);
+		if(Math.abs(coefficient) < 0.5) paraDistance2 = getDistance(Li[0], projPoint);
+		else paraDistance2 = getDistance(Li[1],projPoint);
+	} else {
+		perpDistance1 = getDistanceFromPointToLineSegment(Lj,Li[0]);
+		if(Math.abs(coefficient) < 0.5) paraDistance1 = getDistance(Lj[0], projPoint);
+		else paraDistance1 = getDistance(Lj[1],projPoint);
+		perpDistance2 = getDistanceFromPointToLineSegment(Lj,Li[1]);
+		if(Math.abs(coefficient) < 0.5) paraDistance2 = getDistance(Lj[0], projPoint);
+		else paraDistance2 = getDistance(Lj[1],projPoint);
+	}
+	if(!(perpDistance1 == 0.0 && perpDistance2 == 0.0)){
+		perpDistance = ((Math.pow(perpDistance1,2) + Math.pow(perpDistance2,2))/(perpDistance1+perpDistance2));
+	} else {
+		perpDistance = 0.0;
+	}
+	//Compute Parallel Distance
+	paraDistance = (paraDistance1 < paraDistance2) ? paraDistance1 : paraDistance2;
+	//Compute Angle Distance
+	if(len1 > len2)
+		angleDistance = calcAngularDistance(Li,Lj);
+	else
+		angleDistance = calcAngularDistance(Lj,Li);
+	
+	return (perpDistance + paraDistance + angleDistance);
 }
 
 /*
@@ -466,7 +482,7 @@ function Ne1(D,index,e,C,clusterId){
 	for(var i=0; i<D.length; i++){
 		if(i != index && !D[i].classified){
 			//Get distance
-			var d = dist(D[index].L,D[i].L,1,1,1);
+			var d = dist(D[index].L,D[i].L);
 			//Add line if distance is less than or equal to e
 			if(d <= e){
 				//Assign cluster id to new line in cluster
@@ -486,7 +502,7 @@ function Ne2(D,index,e){
 	for(var i=0; i<D.length; i++){
 		if(i != index && !D[i].classified){
 			//Get distance
-			var d = dist(D[index].L,D[i].L,1,1,1);
+			var d = dist(D[index].L,D[i].L);
 			//Add line if distance is less than or equal to e
 			if(d <= e) N.push(D[i]);
 		}
@@ -505,7 +521,7 @@ function rotateVector(v,angle){
 	x2 = v.L[1][0]*Math.cos(radians) - v.L[1][1]*Math.sin(radians); 
 	y1 = v.L[0][0]*Math.sin(radians) + v.L[0][1]*Math.cos(radians); 
 	y2 = v.L[1][0]*Math.sin(radians) + v.L[1][1]*Math.cos(radians);
-	return {"L":[[x1,y1],[x2,y2]],"trajectory":v.trajectory,"clusterId":v.clusterId,"classified":v.classified,"noise":v.noise,"index":v.index};
+	return {"L":[[x1,y1],[x2,y2]],"trajectory":v.trajectory,"clusterId":v.clusterId,"classified":v.classified,"noise":v.noise};
 }
 /*Rotate matrix counter-clockwise*/
 function rotateMatrix(m,angle){
