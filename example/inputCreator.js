2dbdb5846130a7a7bc97eb70867c55a881dbc089
astrelke <Strelke.Andrew@gmail.com>
2018-04-23 20:38:45 -0400

    Update inputCreator.js

--- example/inputCreator.js
+++ example/inputCreator.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var data = ''; var textArray; var result; var coordinates = [], point=[0.0,0.0], features = []; var feature; var nan;
 var readStream = fs.createReadStream('./BestTrackAtlantic.txt', 'utf8');
+var minL=0, maxL=0, minH=0, maxH=0;
 readStream.on('data', function(chunk) {  
     data += chunk;
 }).on('end', function() {
@@ -8,21 +9,54 @@ readStream.on('data', function(chunk) {
 	for(var i=1; i<textArray.length; i++){
 		result = textArray[i].substring(0, 4).trim();
 		if(result === 'EP' || result === 'CP' || result === 'AL'){
-			console.log(result);
-			feature = {"type":"Feature", "geometry":{"type":"LineString", "coordinates":coordinates}, "properties":{"name":textArray[i].trim()}};
+			feature = {"type":"Feature", "geometry":{"type":"LineString", "coordinates":coordinates}, "properties":{"name":textArray[i].trim(), 'density':1}};
 			features.push(feature);
 			coordinates = [];
 		} else{
 			nan = isNaN(textArray[i].slice(1,-1).trim());
+			//North
 			if(textArray[i].slice(-1).trim() == 'N' && !nan){
 				point[1] = parseFloat(textArray[i].slice(1, -1).trim());
+				if(point[1] > maxH)
+					maxH = point[1];
+			//South
+			} else if(textArray[i].slice(-1).trim() == 'S' && !nan){
+				point[1] = parseFloat("-"+textArray[i].slice(1, -1).trim());
+				if(point[1] < minH)
+					minH = point[1];
+			//East
+			} else if(textArray[i].slice(-1).trim() == 'E' && !nan){
+				point[0] = parseFloat(textArray[i].slice(1, -1).trim());
+				//Check if coordinate is actually West
+				if(point[0] > 180){
+					point[0] -= 360;
+					if(point[0] < minL)
+						minL = point[0];
+				} else {
+					if(point[0] > maxL)
+						maxL = point[0];
+				}
+				coordinates.push(point);
+				point = [0.0,0.0];
+			//West
 			} else if(textArray[i].slice(-1).trim() == 'W' && !nan){
 				point[0] = parseFloat("-"+textArray[i].slice(1, -1).trim());
+				//Check if coordinate is actually East
+				if(point[0] < -180){
+					point[0] += 360;
+					if(point[0] > maxL)
+						maxL = point[0];
+				} else {
+					if(point[0] < minL)
+						minL = point[0];
+				}
 				coordinates.push(point);
 				point = [0.0,0.0];
 			}
 		}
 	}
+	console.log("Height: " + maxH + "N " + minH + "S");
+	console.log("Length: " + maxL + "E " + minL + "W");
 	var json= {"type":"FeatureCollection","features":features};
 	var fileName = 'BestTrackAtlantic.geojson';
 	fs.writeFileSync(fileName, JSON.stringify(json));	
