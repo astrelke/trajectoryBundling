9c3be5cb18009061a9031a90da2cdfc2a6a35f8a
astrelke <Strelke.Andrew@gmail.com>
2018-09-02 16:47:26 -0400

    Update geojson2mvt.js

--- example/geojson2mvt.js
+++ example/geojson2mvt.js
@@ -23,49 +23,45 @@ var end = myArgs[0].length;
 //Create vector tiles for each level
 for(var i=0; i<=parseInt(myArgs[5]); i++){
 	//Generate First Tile Set
-	json = JSON.parse(fs.readFileSync(myArgs[0]+'('+i+').geojson', "utf8"));
-	var options = {
-	  layers: {
-		layer0: json,
-	  },
-	  rootDir: "Tiles/"+myArgs[0].substring(start,end)+"(1)",
-	  //bbox : [0,-110,81,63], //[south,west,north,east]
-	  bbox : [parseFloat(myArgs[1]),parseFloat(myArgs[2]),parseFloat(myArgs[3]),parseFloat(myArgs[4])],	//[south,west,north,east]
-	  zoom : {
-		min :i,
-		max :i
-	  },
-	  tolerance : 0
-	  //tolerance : myArgs[6]
-	};
-	console.time('geojson2mvt'+i.toString());
-	// build the static tile pyramid
-	geojson2mvt(options);
-	console.timeEnd('geojson2mvt'+i.toString());
-	
-	//Generate Second Tile Set
-	var j = i+1;
-	if(j <= parseInt(myArgs[5])){
-		json2 = JSON.parse(fs.readFileSync(myArgs[0]+'('+j+').geojson', "utf8"));
-		var options2 = {
+	if(i < parseInt(myArgs[5])){
+		json = JSON.parse(fs.readFileSync(myArgs[0]+'('+i+').geojson', "utf8"));
+		var options = {
 		  layers: {
-			layer0: json2,
+			layer0: json,
 		  },
-		  rootDir: "Tiles/"+myArgs[0].substring(start,end)+"(2)",
-		  //bbox : [0,-110,81,63], //[south,west,north,east]
+		  rootDir: "Tiles/"+myArgs[0].substring(start,end)+"(1)",
 		  bbox : [parseFloat(myArgs[1]),parseFloat(myArgs[2]),parseFloat(myArgs[3]),parseFloat(myArgs[4])],	//[south,west,north,east]
 		  zoom : {
 			min :i,
 			max :i
 		  },
 		  tolerance : 0
-		  //tolerance : myArgs[6]
 		};
-		console.time('geojson2mvt'+j.toString());
+		console.time('geojson2mvt'+i.toString());
 		// build the static tile pyramid
-		geojson2mvt(options2);
-		console.timeEnd('geojson2mvt'+j.toString());
+		geojson2mvt(options);
+		console.timeEnd('geojson2mvt'+i.toString());
 	}
+	//Generate Second Tile Set
+	var j = i+1;
+	if(j > parseInt(myArgs[5])) j = i;
+	json2 = JSON.parse(fs.readFileSync(myArgs[0]+'('+j+').geojson', "utf8"));
+	var options2 = {
+	  layers: {
+			layer0: json2,
+		},
+		rootDir: "Tiles/"+myArgs[0].substring(start,end)+"(2)",
+		bbox : [parseFloat(myArgs[1]),parseFloat(myArgs[2]),parseFloat(myArgs[3]),parseFloat(myArgs[4])],	//[south,west,north,east]
+		zoom : {
+			min :i,
+			max :i
+		},
+		 tolerance : 0
+	};
+	console.time('geojson2mvt'+j.toString());
+	// build the static tile pyramid
+	geojson2mvt(options2);
+	console.timeEnd('geojson2mvt'+j.toString());
 }
 
 
